316a2be289c54f647c4e19fff0caf7b4902100d0
SIMPLEPLAN <sachinsaini794@gmail.com>
2025-02-20 18:33:29 +0530

    product ui setup

---
+++ app/index.tsx
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import products from '../assets/products.json'
+import ProductListItem from '../components/ProductListItem';
+
+export default function HomeScreen() {
+  return (
+    <FlatList 
+      data={products} 
+      numColumns={2}
+      contentContainerClassName='gap-2'
+      columnWrapperClassName='gap-2'
+      renderItem={({ item }) => <ProductListItem product={item} />}
+      keyExtractor={(item) => item.id.toString()}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
