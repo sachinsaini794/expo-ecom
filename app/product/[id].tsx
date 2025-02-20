82847099ee2153cb695cd168007a45d3979351e9
sachinsaini794 <sachinsaini794@gmail.com>
2025-02-20 19:14:49 +0530

    product details screen

--- app/product/[id].tsx
+++ app/product/[id].tsx
@@ -1,3 +1,5 @@
+import { useLocalSearchParams } from "expo-router"
+import products from '@/assets/products.json'
 import { Box } from "@/components/ui/box"
 import { Button, ButtonText } from "@/components/ui/button"
 import { Card } from "@/components/ui/card"
@@ -5,22 +7,20 @@ import { Heading } from "@/components/ui/heading"
 import { Image } from "@/components/ui/image"
 import { Text } from "@/components/ui/text"
 import { VStack } from "@/components/ui/vstack"
-import { Link } from "expo-router"
-import { Pressable } from "@/components/ui/pressable"
 
 
-interface Product {
-  id: number;
-  name: string;
-  description: string;
-  image: string;
-  price: number;
-}
+const ProductDetailScreen = () => {
 
-export default function ProductListItem({ product }: { product: Product }) {
-  return <Link href={`product/${product.id}`} asChild>
-    <Pressable className="flex-1">
-    <Card className="p-5 rounded-lg max-w-[360px] flex-1">
+  const { id } = useLocalSearchParams<{ id: string }>()
+
+  const product = products.find((p) => p.id == Number(id));
+
+  if (!product) {
+    return <Text>Product not found</Text>
+  }
+
+  return (
+    <Card className="p-5 rounded-lg max-w-[560px] flex-1">
       <Image
         source={{
           uri: product.image,
@@ -32,15 +32,29 @@ export default function ProductListItem({ product }: { product: Product }) {
       <Text className="text-sm font-normal mb-2 text-typography-700">
         {product.name}
       </Text>
+      <VStack className="mb-6">
         <Heading size="md" className="mb-4">
           ${product.price}
         </Heading>
+        <Text size="sm">
+          {product.description}
+        </Text>
+      </VStack>
       <Box className="flex-col sm:flex-row">
         <Button className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
           <ButtonText size="sm">Add to cart</ButtonText>
         </Button>
+        <Button
+          variant="outline"
+          className="px-4 py-2 border-outline-300 sm:flex-1"
+        >
+          <ButtonText size="sm" className="text-typography-600">
+            Wishlist
+          </ButtonText>
+        </Button>
       </Box>
     </Card>
-    </Pressable>
-  </Link>
+  )
 }
+
+export default ProductDetailScreen
